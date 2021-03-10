94c88f53ebaaf024ef27488c81ad076f30994463
kevin-tolentino <kevin.tolentino95@gmail.com>
2021-03-09 16:24:14 -0800

    added pseudo code commented to explain tasks of implementation

--- main.go
+++ main.go
@@ -18,41 +18,43 @@ var errInvalidArguments = errors.New("Invalid arguments!")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-	if len(os.Args) != 2 {
-		printError(errInvalidArguments)
+	if len(os.Args) != 2 { //check if the arguments are not 2
+		printError(errInvalidArguments) // invoke to prompt that arguments are invalid
 	}
 
-	originUnit = strings.ToUpper(os.Args[1])
-	fmt.Println(" line 26!!Origin Unit:", originUnit)
+	originUnit = strings.ToUpper(os.Args[1]) // call method for consistency sake of user input and assign to originUnit
+	//fmt.Println(" line 26!!Origin Unit:", originUnit)
 
 	for {
-		if !(originUnit == "C" || originUnit == "F") {
-			printError(errInvalidArguments)
+		if originUnit != "C" || originUnit != "F" { //why doesn't this work?
+			//if !(originUnit == "C" || originUnit == "F") { // my own implementation to check that argument is C or F for unit
+			printError(errInvalidArguments) // invoke error message if not so
 		}
-
+//Start with the positive case first (think of it like a unit test)
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-		_, err = fmt.Scanln(&originValue)
-		fmt.Println(" line 35!!Origin Value:", originValue)
+		_, err = fmt.Scanln(&originValue) // read the input and
+		//& tells the runner to pass by ref rather than value
+		//fmt.Println(" line 35!!Origin Value:", originValue)
 
-		if err != nil {
+		if err != nil { //if not a float64 type
 			printError(errReadingInput)
 		}
 
-		if originUnit == "C" {
-			convertToFahrenheit(originValue)
+		if originUnit == "C" { // if the originUnit was originally C unit
+			convertToFahrenheit(originValue) //convert the value to fahrenheit
 		} else {
-			convertToCelsius(originValue)
+			convertToCelsius(originValue) // if not C unit, convert value to celsius
 		}
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
-		_, err = fmt.Scanln(&shouldConvertAgain)
+		_, err = fmt.Scanln(&shouldConvertAgain) // read line
 
-		if err != nil {
-			printError(errReadingInput)
+		if err != nil { //if any error, print error
+			printError(errReadingInput) //
 		}
 
-		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" { // check if string y is used
 			fmt.Println("Good bye!")
 			break
 		}
